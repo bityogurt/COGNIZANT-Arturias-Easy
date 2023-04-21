7d0d9a95b649fa7f0be003d101904798559e007f
Alin Anghelus <anghelusalin02@gmail.com>
2023-04-21 19:42:18 +0300

    added hibernate cfg, tested if hibernate can save then retrieve stuff from internal database

---
+++ back-end/src/main/java/ro/cognizant/coderun2023/Book.java
@@ -0,0 +1,25 @@
+package ro.cognizant.coderun2023;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "book")
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    @Column(name = "bookName")
+    private String bookName;
+
+    @Column(name = "author")
+    private String author;
+
+    @Column(name = "publishers")
+    private String publishers;
+
+}
